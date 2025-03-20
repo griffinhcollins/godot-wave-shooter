7baac1e372c230123f0e66e9d29be83054b49677
Griffin <gcol0008@student.monash.edu>
2025-03-20 19:22:46 +1100

    Basic coins

---
+++ Coin.cs
@@ -0,0 +1,41 @@
+using Godot;
+using System;
+
+public partial class Coin : Area2D
+{
+
+
+
+    int value = 5;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+
+        GetNode<AnimatedSprite2D>("AnimatedSprite2D").Animation = "default";
+        GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play();
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
+
+
+    private void OnTouchPlayer(Area2D touchedArea)
+    {
+        Player player;
+        if (touchedArea.IsInGroup("player"))
+        {
+            player = (Player)touchedArea;
+        }
+        else
+        {
+            return;
+        }
+
+        player.AddMoney(value);
+        QueueFree();
+
+    }
+
+}

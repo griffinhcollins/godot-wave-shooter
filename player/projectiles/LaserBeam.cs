63f201b1c269827a14908e9dccdc4d10d7bfbf07
griffinhcollins <griffinhoycollins@gmail.com>
2025-05-15 12:32:49 +1000

    Got laser beam working in conjunction with other projectiles

--- player/projectiles/LaserBeam.cs
+++ player/projectiles/LaserBeam.cs
@@ -2,50 +2,66 @@ using Godot;
 using static Stats.PlayerStats;
 using System;
 
-public partial class LaserBeam : Area2D
+public partial class LaserBeam : Bullet
 {
 
 	GpuParticles2D particles;
 	CollisionShape2D hitbox;
 
-	double lifeLeft;
 
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		particles = GetNode<GpuParticles2D>("BeamParticles");
-		hitbox = GetNode<CollisionShape2D>("CollisionShape2D");
+		base._Ready();
+		Node2D parent = GetParent<Node2D>();
+		particles = parent.GetNode<GpuParticles2D>("BeamParticles");
+		hitbox = parent.GetNode<CollisionShape2D>("CollisionShape2D");
 
 		RectangleShape2D collisionShape = new RectangleShape2D();
-		collisionShape.Size = new Vector2(20, 400 * ShotSpeed.GetDynamicVal());
+		collisionShape.Size = new Vector2(20 * BulletSize.GetDynamicVal(), 400 * ShotSpeed.GetDynamicVal());
 
 		hitbox.Shape = collisionShape;
 
-		lifeLeft = Unlocks.LaserLifetime.GetDynamicVal();
 
 	}
 
 
-	private void OnHit(Node2D body){
+	private void OnHit(Node2D body)
+	{
 		if (body.IsInGroup("mobs"))
-        {
-            Mob mobHit = (Mob)body;
-            mobHit.TakeDamage(Damage.GetDynamicVal());
-        }
+		{
+			Mob mobHit = (Mob)body;
+			mobHit.TakeDamage(Damage.GetDynamicVal());
+		}
 	}
 
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-	
-		lifeLeft -= delta;
+		base._Process(delta);
 		// Introduce a bit of randomness with how long lasers last makes them look a bit less jank with multishot
-		if (lifeLeft <= 0 && GD.Randi() % 3 == 0)
+		if (timeAlive >= Unlocks.LaserLifetime.GetDynamicVal() && GD.Randi() % 3 == 0)
 		{
 			particles.Hide();
-			QueueFree();
+			HandleDeath();
 		}
 	}
+
+	protected override Vector2 GetCurrentVelocity()
+	{
+		return Vector2.Up.Rotated(GetParent<Node2D>().Rotation);
+	}
+
+	protected override void Pause()
+	{
+		return;
+	}
+
+	protected override void UnPause()
+	{
+		return;
+	}
+
 }

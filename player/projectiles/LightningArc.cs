ad52b021470dd92871fb2c4bb093ef5d1a78e47a
griffinhcollins <griffinhoycollins@gmail.com>
2025-05-16 18:12:15 +1000

    messed around with adding lightning animation, kinda cool but doesn't make sense for lightning so sticking with single texture

--- player/projectiles/LightningArc.cs
+++ player/projectiles/LightningArc.cs
@@ -3,17 +3,37 @@ using System;
 
 public partial class LightningArc : Node2D
 {
+	[Export]
+	Texture2D lightningOne;
+	[Export]
+	Texture2D lightningTwo;
+	[Export]
+	Texture2D lightningThree;
+
+	Line2D arcLine;
+	double timeAlive;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		arcLine = GetNode<Line2D>("Arc");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		// timeAlive += delta;
+		// if (timeAlive > 0.03f)
+		// {
+		// 	arcLine.Texture = lightningTwo;
+		// }
+		// if (timeAlive > 0.06f)
+		// {
+		// 	arcLine.Texture = lightningThree;
+		// }
 	}
 
-	void OnTimerComplete(){
+	void OnTimerComplete()
+	{
 		QueueFree();
 	}
 }
